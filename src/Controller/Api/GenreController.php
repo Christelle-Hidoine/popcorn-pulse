4f413bc3c0b88727e6f109909dbb909b606a10ea
Christelle-Hidoine <christelle.hidoine@gmail.com>
2023-05-22 13:04:48 +0200

    correction event et ajout event UserType et condition sur le message de maintenance paramétré dans le fichier .env

---
+++ src/Controller/Api/GenreController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GenreController extends AbstractController
+{
+    /**
+     * @Route("/api/genres", name="app_api_genres")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/Api/GenreController.php',
+        ]);
+    }
+}
